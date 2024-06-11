f04a074fd4a08510ed94cd9b93c4e43e918e9289
CoderN-P <neel.parpia@gmail.com>
2024-06-11 08:56:26 -0700

    Update build and meta info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NextAuthProvider } from "./providers";
+import Head from 'next/head';
+
 
 export default function RootLayout({
   children,
@@ -8,6 +10,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <Head>
+            <meta charSet="utf-8" />
+            <title>NextChat</title>
+            <meta name="og:title" content="NextChat" />
+            <meta name="description" content="A ssimple but modern chat application built with Next.js and MongoDB" />
+            <meta name="og:description" content="A simple but modern chat application built with Next.js and MongoDB" />
+            <meta name="og:image" content="/NextChat.png" />
+            <meta name="theme-color" content="#000000" />
+            <meta name="robots" content="index, follow" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
       <body>
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
